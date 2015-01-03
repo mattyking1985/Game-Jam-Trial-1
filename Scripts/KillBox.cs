d43937d01897c3b9221888bec4d482981fafb89e
noctisyounis <cherifyounis@gmail.com>
2015-01-03 22:43:24 +0100

    dead zone and cleaning scene

---
+++ Scripts/KillBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
